c0766ddfee005e968d71fd0b9266ade875f92801
Roman <roman.khadka13@gmail.com>
2025-04-20 00:15:06 -0600

    feat(benchmark): make samples parameter configurable in Dhrystone script

--- run_dhry_parallel.sh
+++ run_dhry_parallel.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# Default number of samples if not specified
+SAMPLES=${1:-5}
+
 # How many cores to use
 CORES=$(sysctl -n hw.ncpu)
 
 # Record start time
 START_TIME=$(date +%s)
-echo "Starting Dhrystone benchmark across $CORES cores..."
+echo "Starting Dhrystone benchmark across $CORES cores with $SAMPLES samples per instance..."
 
 # Run one benchmark process per core
 for i in $(seq 1 $CORES); do
   echo "Starting Dhrystone instance $i"
-  cargo run -p threadstone-cli -- run -w dhrystone --threads 1 --samples 5 &
+  cargo run -p threadstone-cli -- run -w dhrystone --threads 1 --samples $SAMPLES &
 done
 
 # Wait for all background processes to complete
